a4d13d3e29c17f48e43640dfffc452022e448d22
robotemperor <yik@mmmi.sdu.dk>
2020-10-08 13:51:28 +0200

    testing

--- disassembly.py
+++ disassembly.py
@@ -3,33 +3,36 @@ from rtde_control import RTDEControlInterface
 from rtde_receive import RTDEReceiveInterface
 from transform3d import Transform
 
-r_b = RTDEControlInterface("192.168.1.130")
-r_b.ctrl.moveL(r_b.base_t_tcp() @ Transform(rotvec=(0, 0, np.pi)))
+r_a = RTDEControlInterface("192.168.1.129")
+r_a.ctrl.moveL(r_a.base_t_tcp() @ Transform(rotvec=(0, 0, 0)))
 
-task_frame = [0, 0, 0, 0, 0, 0]
-selection_vector = [0, 0, 1, 0, 0, 0]
-wrench_down = [0, 0, -10, 0, 0, 0]
-wrench_up = [0, 0, 10, 0, 0, 0]
-force_type = 2
-limits = [2, 2, 1.5, 1, 1, 1]
-dt = 1.0/500  # 2ms
-joint_q = [-1.54, -1.83, -2.28, -0.59, 1.60, 0.023]
+pulley_frame = [0, 0, 0, 0, 0, 0]
 
-# Move to initial joint position with a regular moveJ
-rtde_c.moveJ(joint_q)
+big_pulley = self._taskboard.find_frame("big_pulley")
+small_pulley = self._taskboard.find_frame("small_pulley")
 
-# Execute 500Hz control loop for 4 seconds, each cycle is 2ms
-for i in range(2000):
-    start = time.time()
-    # First move the robot down for 2 seconds, then up for 2 seconds
-    if i > 1000:
-        rtde_c.forceMode(task_frame, selection_vector, wrench_up, force_type, limits)
+
+
+
+def disassembly_place_belt(self, obj: Object, fingers: Tool):
+    pick_frame = obj.find_frame("pick")
+    if fingers is self._fingers_1:
+        tool_frame = fingers.find_frame("grasp")
+    elif fingers is self._fingers_6:
+        tool_frame = fingers.find_frame("grasp_inner")
     else:
-        rtde_c.forceMode(task_frame, selection_vector, wrench_down, force_type, limits)
-    end = time.time()
-    duration = end - start
-    if duration < dt:
-        time.sleep(dt - duration)
+        raise Exception("Expected use of fingers 1 or 6.")
 
-rtde_c.forceModeStop()
-rtde_c.stopScript()
+    pick = PickObject(self._robot_a_control, self._robot_a_receive, self._gripper_a, fingers, tool_frame, obj,
+                      pick_frame, 0.01, 0.002, self._persistence)
+
+    taskboard_frame = self._taskboard.find_frame(obj.get_name())
+
+
+    self._robot_a_control.move_home(self._speed_a, self._acc_a)
+    self.check_frame(obj, self._kitlayout.find_frame(obj.get_name()))
+    pick.pick(0.2)
+
+    release_width = pick.get_release_width()
+    self._gripper_a.release(release_width, 255, 255)
+    self._robot_a_control.move_home(self._speed_a, self._acc_a)
